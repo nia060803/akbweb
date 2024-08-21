9e5152d454c7a8d6322fa1b3f19d8fa0812e7906
creamynald <creamynald@gmail.com>
2024-08-21 11:08:33 +0700

    add: init module Instansi

---
+++ app/Http/Controllers/backend/pengaturan/instansiController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\backend\pengaturan;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class instansiController extends Controller
+{
+    //
+}
